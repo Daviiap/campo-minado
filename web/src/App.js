01fdd116f9b77d4e6907a1ac259baef175d9128c
Davi Andrade Pontes <daviiap@outlook.com>
2021-09-02 18:52:39 -0300

    Iniciando conexao do frontend com o backend

--- web/src/App.js
+++ web/src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { io } from "socket.io-client";
+
 
 function App() {
+  const [socket, setSocket] = useState(null);
+  const [table, setTable] = useState(null);
+
+  useEffect(() => {
+    if (!socket) {
+      setSocket(io("http://localhost:3000/"));
+    } else {
+      socket.on('connect', () => {
+        console.log(`Your id: ${socket.id}`);
+        socket.on("gameInitiated", (gameState) => {
+          if (!table) {
+            setTable(gameState);
+          }
+        });
+      });
+
+      console.table(table);
+    }
+  }, [socket, table]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
